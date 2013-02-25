eb6877adb2d3e90a6f4fa3ad0857d025ed27c96c
Kris Linquist <kris@linquist.net>
2013-02-24 16:30:21 -0800

    Added 'send sms how often' feature for reassociations

--- aironetmonitor.js
+++ aironetmonitor.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var redis = require('redis');
-
+var howoften = 43200;  //  alert every 12 hours
 var fs = require('fs');
 eval(fs.readFileSync('config.js') + '');
 
@@ -10,7 +10,7 @@ function sendsms(message) {
         from: TwilioFromNumber,
         body: message
     });
-    console.log("sending SMS to " + TwilioToNumber + ": " + message);
+    console.log("Sending SMS to " + TwilioToNumber + ": " + message);
 }
 
 function addWorker() {
@@ -26,27 +26,32 @@ function addWorker() {
                 var mac = homeevents.payload.data.match(/Station(.*?)Associated/);
                 if (mac) {   //Make sure the RegEx returns something
                 	mac = mac[1].trim();  
+                	var ts = Math.round((new Date()).getTime() / 1000);
                     connection.query('SELECT * from mactable where `mac` = \'' + mac + '\'', function (err, rows, fields) {
                         if (err) console.log("Mysql error while looking through mac table: " + err.code);
                         if (rows.length == "0") {
                             console.log("New device detected on home network: " + mac);
                             sendsms("New device detected on home network: " + mac);
-                            connection.query('INSERT INTO mactable VALUES (\'' + mac + '\', now(), \'\', \'\')');
+                            connection.query('INSERT INTO mactable VALUES (\'' + mac + '\', \'' + ts + '\', \'\', \'\')');
                         } else {
                             if (rows[0].report !== "N") {
                                 if (rows[0].devicename !== "") {
                                     console.log("Device detected on home network: " + rows[0].devicename + " (" + mac + ")");
-                                    sendsms("Device detected on home network: " + rows[0].devicename + " (" + mac + ")");
+                                    if (ts - parseInt(rows[0].lastseen) > howoften) {
+	                                    sendsms("Device detected on home network: " + rows[0].devicename + " (" + mac + ")");
+	                                }
                                 } else {
                                     console.log("Device detected on home network: " + mac);
-                                    sendsms("Device detected on home network: " + mac);
+                                    if (ts - parseInt(rows[0].lastseen) > howoften) {
+                                    	sendsms("Device detected on home network: " + mac);
+                                    }
                                 }
                             } else {
                                 console.log("Device detected on home network: " + rows[0].devicename + " (" + mac + ")");
                             }
                         }
                     });
-                    connection.query('UPDATE mactable SET lastseen = now() WHERE mac = \'' + mac + '\'', function (err) {
+                    connection.query('UPDATE mactable SET lastseen = \'' + ts + '\' WHERE mac = \'' + mac + '\'', function (err) {
                         if (err) console.log("Mysql error trying to update last seen table: " + err.code);
                     });
                 }
